6d215eb8e44b4e56da05ffa95c8053508d2fb4b4
vjpal3 <vrishalipal@yahoo.com>
2019-09-21 18:29:16 -0400

    Detect palindrome string using stack and queue

---
+++ StackQueueDay18.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution 
+{
+    //Write your code here
+    List<char> stack = new List<char>();
+    List<char> queue = new List<char>();
+
+    public void pushCharacter(char ch)
+    {
+        stack.Insert(0, ch);
+    }
+
+    public char popCharacter()
+    {
+        char ch = stack[0];
+        stack.RemoveAt(0);
+        return ch;
+    }
+
+    public void enqueueCharacter(char ch) 
+    {
+        queue.Add(ch);
+    }
+
+    public char dequeueCharacter()
+    {
+        char ch = queue[0];
+        queue.RemoveAt(0);
+        return ch;
+    } 
+    
+    static void Main(String[] args) 
+    {
+        // read the string s.
+        string s = Console.ReadLine();
+        
+        // create the Solution class object p.
+        Solution obj = new Solution();
+        
+        // push/enqueue all the characters of string s to stack.
+        foreach (char c in s) 
+        {
+            obj.pushCharacter(c);
+            obj.enqueueCharacter(c);
+        }
+        
+        bool isPalindrome = true;
+        
+        // pop the top character from stack.
+        // dequeue the first character from queue.
+        // compare both the characters.
+        for (int i = 0; i < s.Length / 2; i++) 
+        {
+            if (obj.popCharacter() != obj.dequeueCharacter()) 
+            {
+                isPalindrome = false;
+                
+                break;
+            }
+        }
+        
+        // finally print whether string s is palindrome or not.
+        if (isPalindrome) 
+        {
+            Console.Write("The word, {0}, is a palindrome.", s);
+        } 
+        else 
+        {
+            Console.Write("The word, {0}, is not a palindrome.", s);
+        }
+    }
+}
