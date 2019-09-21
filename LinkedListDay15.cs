95eca8bb3006d6e53d39392a3ffd6009a81bbf0f
vjpal3 <vrishalipal@yahoo.com>
2019-09-21 16:47:26 -0400

    Insert node in Linked List

---
+++ LinkedListDay15.cs
@@ -0,0 +1,59 @@
+using System;
+class Node
+{
+	public int data;
+	public Node next;
+    
+    public Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+class Solution {
+
+   public static  Node insert(Node head,int data)
+   {
+        //Complete this method
+        Node newNode = new Node(data);
+        if(head == null) 
+	    {
+            head = newNode;
+        }
+        else 
+        {
+            Node temp = head;
+            while (temp.next != null) 
+	        {
+                temp = temp.next;
+            } 
+            temp.next = newNode;
+            newNode.next = null;
+        }
+        return head;
+    }
+
+    public static void display(Node head)
+    {
+		Node start = head;
+		while(start != null)
+		{
+			Console.Write(start.data+" ");
+			start = start.next;
+		}
+    }
+
+    static void Main(String[] args) 
+    {
+		Node head = null;
+        int T=Int32.Parse(Console.ReadLine());
+
+        while(T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            head = insert(head,data);
+        }
+		display(head);
+	}
+}
+	
