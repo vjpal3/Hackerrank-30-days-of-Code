5a625937ad6d521fff079fbacfc913d4e3c9be67
Vrishali Pal <vrishalipal@yahoo.com>
2020-04-26 14:37:30 -0400

    Left Rotation of array elements

---
+++ Java-Hackerrank/InterviewPractice/Arrays/ArrayLeftRot.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  static int[] rotLeft(int[] a, int d) {
+
+    int n = a.length;
+    // reverse first d elements
+    reverse(a, 0, d - 1);
+
+    // reverse remaining n-d elements
+    reverse(a, d, n - 1);
+
+    // reverse the whole array
+    reverse(a, 0, n - 1);
+
+    return a;
+  }
+
+  static void reverse(int[] a, int low, int high) {
+    for (int i = low, j = high; i < j; i++, j--) {
+      int temp = a[i];
+      a[i] = a[j];
+      a[j] = temp;
+    }
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    String[] nd = scanner.nextLine().split(" ");
+
+    int n = Integer.parseInt(nd[0]);
+
+    int d = Integer.parseInt(nd[1]);
+
+    int[] a = new int[n];
+
+    String[] aItems = scanner.nextLine().split(" ");
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    for (int i = 0; i < n; i++) {
+      int aItem = Integer.parseInt(aItems[i]);
+      a[i] = aItem;
+    }
+
+    int[] result = rotLeft(a, d);
+
+    for (int i = 0; i < result.length; i++) {
+      bufferedWriter.write(String.valueOf(result[i]));
+
+      if (i != result.length - 1) {
+        bufferedWriter.write(" ");
+      }
+    }
+
+    bufferedWriter.newLine();
+
+    bufferedWriter.close();
+
+    scanner.close();
+  }
+}
