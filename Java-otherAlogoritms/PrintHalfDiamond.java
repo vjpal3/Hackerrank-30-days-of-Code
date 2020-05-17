ca56e36096f42f088e2d6c7544b7279e53b8b979
Vrishali Pal <vrishalipal@yahoo.com>
2020-05-16 22:34:53 -0400

    Print Half-Diamond Shape

---
+++ Java-otherAlogoritms/PrintHalfDiamond.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    int n = 8;
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+}
