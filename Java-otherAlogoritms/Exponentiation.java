50128916d50ec9232c1128af7e6d44f90223501a
Vrishali Pal <vrishalipal@yahoo.com>
2020-06-28 20:28:43 -0400

    Exponentiation with time complexity of log(n)

--- Java-otherAlogoritms/Exponentiation.java
+++ Java-otherAlogoritms/Exponentiation.java
@@ -2,9 +2,25 @@ public class Exoponentiation{
 
   public static void main(String []args){
      
-     System.out.println(recursivePower(2, -3));
+    //  System.out.println(recursivePower(2, -3));
      // System.out.println(calculatePower(2, -3));
+     System.out.println(efficientPower(2, 5));
   }
+
+  public static long efficientPower(int base, int exp) {
+    if(exp == 0)   
+        return 1;
+    if(exp == 1) 
+        return base;
+    
+    long result = efficientPower(base, exp/2);
+    
+    if(exp % 2 == 0) 
+        return result * result;
+    else 
+        return result * base * result;
+    
+ } 
   
   public static double calculatePower(double base, double exponent) {
       double result = 1;
